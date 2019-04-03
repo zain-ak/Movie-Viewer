5b170ff48832b97c714b842ff95ace1af731e149
Zain Khalid <zak565@uowmail.edu.au>
2019-04-03 02:01:49 -0400

    build.gradle:  - added dependencies for retrofit and gson, which'll allow for easier    networking while retrieving data from TMDb
    Movie.java:
     - Model class created for instances of movies, storing information
       retrieved from TMDb
    
    MovieTest.java:
     - Unit Testing class, only body added as yet
    
    AndroidManifest.xml:
     - Permission to use Internet to retrieve movie data.

--- app/src/main/java/android/learning/movieviewer/data/model/Movie.java
+++ app/src/main/java/android/learning/movieviewer/data/model/Movie.java
@@ -1,13 +1,31 @@
 package android.learning.movieviewer.data.model;
 
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
 public class Movie {
+    @SerializedName("id") @Expose
     private final String id;
+
+    @SerializedName("original_title") @Expose
     private final String name;
+
+    @SerializedName("overview") @Expose
     private final String description;
+
+    @SerializedName("poster_path") @Expose
     private final String posterImageURL;
+
+    @SerializedName("imdb_id") @Expose
     private final String imdbID;
+
+    @SerializedName("release_date") @Expose
     private final String releaseDate;
+
+    @SerializedName("runtime") @Expose
     private final String runtime;
+
+    @SerializedName("vote_average") @Expose
     private final String rating;
 
 
---
+++ app/src/test/java/android/learning/movieviewer/data/model/MovieTest.java
@@ -0,0 +1,7 @@
+package android.learning.movieviewer.data.model;
+
+import static org.junit.Assert.*;
+
+public class MovieTest {
+
+}
