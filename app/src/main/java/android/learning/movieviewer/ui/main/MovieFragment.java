594b5ec137625e50d63806c17ab8c3580729f285
Zain Khalid <zak565@uowmail.edu.au>
2019-10-31 00:03:30 -0400

    Worked around with fragment, added a shape for slider...

--- app/src/main/java/android/learning/movieviewer/ui/main/MovieFragment.java
+++ app/src/main/java/android/learning/movieviewer/ui/main/MovieFragment.java
@@ -3,8 +3,10 @@ package android.learning.movieviewer.ui.main;
 import android.learning.movieviewer.R;
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -20,6 +22,20 @@ public class MovieFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.movie_card, container, false);
+        View view = inflater.inflate(R.layout.movie_card, container, false);
+        view.setOnTouchListener(new View.OnTouchListener() {
+            public boolean onTouch(View v, MotionEvent event) {
+
+                if(event.getAction() == MotionEvent.ACTION_UP){
+                    Toast.makeText(v.getContext(), "Moving up", Toast.LENGTH_SHORT).show();
+                }
+
+                if(event.getAction() == MotionEvent.ACTION_DOWN){
+                    Toast.makeText(v.getContext(), "Moving down", Toast.LENGTH_SHORT).show();
+                }
+                return true;
+            }
+        });
+        return view;
     }
 }
